213673bc045bd6e3426542d8e7123dcd306008a3
翟涛 <370985116@qq.com>
2016-01-18 23:42:51 +0800

    base android DrawerLayout

--- index.android.js
+++ index.android.js
@@ -8,44 +8,42 @@ var React = require('react-native');
 var {
   AppRegistry,
   StyleSheet,
-  Text,
   View,
+  Dimensions,
+  DrawerLayoutAndroid,
 } = React;
+
+var DRAWER_WIDTH_LEFT = 156;
 
 var UIExploer = React.createClass({
   render: function() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
+      // 源码 ./node_modules/react-native/Libraries/Components/DrawerAndroid
+      <DrawerLayoutAndroid
+        drawerPosition={DrawerLayoutAndroid.positions.Left} // 支持Left、Right
+        drawerWidth={Dimensions.get('window').width - DRAWER_WIDTH_LEFT}
+        keyboardDismissMode="none" // 支持none、on-drag 返回是否阻拦
+        ref={(drawer) => { this.drawer = drawer; }}
+        renderNavigationView={this._renderNavigationView}>
+        <View style={styles.navaitor}></View>
+      </DrawerLayoutAndroid>
     );
-  }
+  },
+  _renderNavigationView: function() {
+   return (
+     <View style={styles.drawer}></View>
+   );
+ }
 });
 
 var styles = StyleSheet.create({
-  container: {
+  drawer: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: '#5D9CEC',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  navaitor: {
+    flex: 1,
+    backgroundColor: '#E6E9ED',
   },
 });
 
